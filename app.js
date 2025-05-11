ca0edcb0fc255f2b9d5f09a00004530a2a3a6118
Sandip <154871379+student-sandip@users.noreply.github.com>
2025-05-12 01:43:35 +0530

    commit

--- app.js
+++ app.js
@@ -11,12 +11,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
-mongoose.connect('mongodb://127.0.0.1:27017/snap', { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connect('mongodb://127.0.0.1:27017/snap', { 
+    useNewUrlParser: true, useUnifiedTopology: true
+ })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.log(err));
 require('./config/passport')(passport);  
 app.use(session({
-    secret: process.env.SESSION_SECRET || 'secret',  // Session secret, can be stored in .env
+    secret: process.env.SESSION_SECRET || 'secret',  
     resave: false,
     saveUninitialized: false
 }));
@@ -33,24 +36,22 @@ app.use((req, res, next) => {
 });
 
 
-const indexRoutes = require('./routes/suggestedUsers');  // Home route
-const authRoutes = require('./routes/auth');  // Authentication routes (login, register, logout)
-const postRoutes = require('./routes/post');  // Routes for posts (create, like, comment)
-const storyRoutes = require('./routes/story');  // Routes for stories (create, view)
-const profileRoutes = require('./routes/profile');  // Routes for profile (view, edit)
-const createRoutes = require('./routes/create');  // Routes for chat (view, send)
-const chatRoutes = require('./routes/message');  // Routes for chat (view, send)
+const indexRoutes = require('./routes/suggestedUsers');
+const authRoutes = require('./routes/auth'); 
+const postRoutes = require('./routes/post'); 
+const storyRoutes = require('./routes/story'); 
+const profileRoutes = require('./routes/profile');  
+const createRoutes = require('./routes/create');
+const chatRoutes = require('./routes/message');  
 
-// Use routes
-app.use('/', indexRoutes);  // Home route
-app.use('/profile', profileRoutes);  // Home route
-app.use('/auth', authRoutes);  // Routes for authentication
-app.use('/posts', postRoutes);  // Routes for posts
-app.use('/stories', storyRoutes);  // Routes for stories
-app.use('/create', createRoutes);  // Routes for chat
-app.use('/messages', chatRoutes);  // Routes for chat
+app.use('/', indexRoutes);  
+app.use('/profile', profileRoutes);  
+app.use('/auth', authRoutes);  
+app.use('/posts', postRoutes);  
+app.use('/stories', storyRoutes);
+app.use('/create', createRoutes); 
+app.use('/messages', chatRoutes); 
 
-// Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
